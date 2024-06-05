140415ff5e284c6838d4b52d19c69a1500944c68
Ferdinand Malcher <ferdinand@malcher.media>
2024-06-05 12:41:49 +0200

    BookCOmpoennt und Property Binding

---
+++ book-rating/src/app/books/book/book.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, input } from '@angular/core';
+import { Book } from '../shared/book';
+
+// Aufgabe dieser Komponente:
+// ein einzelnes Buch anzeigen
+@Component({
+  selector: 'app-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './book.component.html',
+  styleUrl: './book.component.scss'
+})
+export class BookComponent {
+  // hier fließen Daten von der Elternkomponente hinein
+  // von oben nach unten
+  @Input() book?: Book;
+}
