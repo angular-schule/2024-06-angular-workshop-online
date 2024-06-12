261d5f37870fbd7363cdacf00438c5e7c85972d3
Ferdinand Malcher <ferdinand@malcher.media>
2024-06-12 11:59:26 +0200

    Typeahead-Suche

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,11 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { BookStoreService } from '../shared/book-store.service';
+import { Observable, debounceTime, filter, of, switchMap } from 'rxjs';
+import { Book } from '../shared/book';
+import { AsyncPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-book-search',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './book-search.component.html',
   styleUrl: './book-search.component.scss'
 })
@@ -13,9 +17,24 @@ export class BookSearchComponent {
   searchControl = new FormControl('', { nonNullable: true });
   private bs = inject(BookStoreService);
 
-  constructor() {
-    this.searchControl.valueChanges.subscribe(e => {
-      console.log(e);
-    });
-  }
+  books2$ = this.searchControl.valueChanges.pipe(
+    debounceTime(200),
+    filter(term => term.length >= 3),
+    switchMap(term => this.bs.search(term))
+  );
+
+  // Alternative
+  books$ = this.searchControl.valueChanges.pipe(
+    debounceTime(200),
+    switchMap(term => {
+      if (term.length >= 3) {
+        return this.bs.search(term);
+      } else {
+        return of([]);
+      }
+    })
+  );
+
+  books = toSignal(this.books$);
+
 }
